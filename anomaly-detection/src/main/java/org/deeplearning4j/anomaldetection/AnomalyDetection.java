9bced52572ab3d332e0298f9f69de9938cadf0ff
naimtenor <naimtenor@gmail.com>
2015-04-09 14:10:36 +0900

    AnomalyDetectionRecorder implementation.

--- anomaly-detection/src/main/java/org/deeplearning4j/anomaldetection/AnomalyDetection.java
+++ anomaly-detection/src/main/java/org/deeplearning4j/anomaldetection/AnomalyDetection.java
@@ -1,5 +1,9 @@
 package org.deeplearning4j.anomaldetection;
 
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.split.FileSplit;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -11,13 +15,20 @@ import java.util.*;
  */
 public class AnomalyDetection {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Map<String, List<Integer>> map = Util.parse(args[0]);
 
-//        System.out.println(map);
-        for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " = " + entry.getValue());
+        RecordReader reader = new AnomalyDetectionRecordReader(map, 50, 50);
+        reader.initialize(new FileSplit(new File(System.getProperty("user.home"), "/dl4j/data/UCSD_Anomaly_Dataset.v1p2/UCSDped1")));
+
+        while (reader.hasNext()) {
+            System.out.println(reader.next());
         }
+
+//        System.out.println(map);
+//        for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + " = " + entry.getValue());
+//        }
     }
 
 
--- anomaly-detection/src/main/java/org/deeplearning4j/anomaldetection/AnomalyDetectionRecordReader.java
+++ anomaly-detection/src/main/java/org/deeplearning4j/anomaldetection/AnomalyDetectionRecordReader.java
@@ -1,13 +1,21 @@
 package org.deeplearning4j.anomaldetection;
 
+import org.canova.api.io.data.IntWritable;
 import org.canova.api.records.reader.RecordReader;
+import org.canova.api.split.FileSplit;
 import org.canova.api.split.InputSplit;
 import org.canova.api.writable.Writable;
+import org.canova.common.RecordConverter;
+import org.canova.image.loader.ImageLoader;
+import org.nd4j.linalg.api.ndarray.INDArray;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
+import java.net.URI;
+import java.util.*;
 
 /**
+ * Custom record reader for
  * Anomaly Detection Record reader<br>
  *     implements RecordReader
  *
@@ -16,23 +24,84 @@ import java.util.Collection;
  */
 public class AnomalyDetectionRecordReader implements RecordReader {
 
-    @Override
-    public void close() throws IOException {
+    private static final String DIRECTORY_NAME_GT = "_gt";
 
+    private Iterator<File> data;
+
+    private Map<String, Boolean> label = new HashMap<>();
+
+    private Map<String, List<Integer>> anomailies;
+
+    private ImageLoader loader;
+
+    /**
+     * defalut constructor
+     * using root directory
+     * and anomaily list..
+     *
+     * @param anomailies Anomal data lists
+     */
+    public AnomalyDetectionRecordReader(Map<String, List<Integer>> anomailies, int rows, int cols) {
+        this.anomailies = anomailies;
+        loader = new ImageLoader(rows, cols);
     }
 
     @Override
     public void initialize(InputSplit split) throws IOException, InterruptedException {
+        if (split instanceof FileSplit) {
+            FileSplit fileSplit = (FileSplit) split;
+            URI[] u = fileSplit.locations();
+            List<File> data = new ArrayList<>();
 
+            for (URI file : u) {
+                File inputFile = new File(file);
+                if (!inputFile.exists()) {
+                    throw new IllegalStateException("Unable to find file " + inputFile.getAbsolutePath());
+                }
+                if (inputFile.isDirectory()) {
+
+                } else {
+                    File video = inputFile.getParentFile();
+                    String videoName = video.getName();
+                    // if directory name not containe _gt...
+                    if (!videoName.contains(DIRECTORY_NAME_GT)) {
+                        String fileName = inputFile.getName();
+                        try {
+                            int clip = Integer.parseInt(fileName.replaceAll(".tif", ""));
+                            boolean anomaly = !anomailies.containsKey(videoName) || anomailies.get(videoName).contains(clip);
+                            label.put(fileName, anomaly);
+                            data.add(inputFile);
+                        } catch (NumberFormatException e) {
+                            continue;
+                        }
+                    }
+                }
+            }
+            this.data = data.iterator();
+        }
     }
 
     @Override
     public Collection<Writable> next() {
+        File next = data.next();
+        try {
+            INDArray row = loader.asRowVector(next);
+            Collection<Writable> record = RecordConverter.toRecord(row);
+            record.add(new IntWritable(label.get(next.getName()) ? 1 : 0));
+            return record;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return data.hasNext();
+    }
+
+    @Override
+    public void close() throws IOException {
+
     }
 }
