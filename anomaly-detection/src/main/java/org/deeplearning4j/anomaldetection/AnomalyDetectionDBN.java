03b494fce7099700d450f19505dd9e131a8270c8
naimtenor <naimtenor@gmail.com>
2015-04-09 16:59:00 +0900

    20150409 last

--- anomaly-detection/src/main/java/org/deeplearning4j/anomaldetection/AnomalyDetectionDBN.java
+++ anomaly-detection/src/main/java/org/deeplearning4j/anomaldetection/AnomalyDetectionDBN.java
@@ -6,8 +6,12 @@ import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.deeplearning4j.models.featuredetectors.rbm.RBM;
 import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.override.ClassifierOverride;
+import org.deeplearning4j.nn.conf.preprocessor.ZeroMeanAndUnitVariancePrePreProcessor;
 import org.deeplearning4j.nn.layers.factory.LayerFactories;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.util.SerializationUtils;
@@ -17,7 +21,8 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 import static java.lang.Integer.MAX_VALUE;
 
@@ -27,7 +32,7 @@ import static java.lang.Integer.MAX_VALUE;
  * @author  naimtenor
  * Created by ubuntu on 15. 4. 9.
  */
-public class AnomalyDetection {
+public class AnomalyDetectionDBN {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Map<String, List<Integer>> map = Util.parse(args[0]);
@@ -51,29 +56,31 @@ public class AnomalyDetection {
 //            DataSet normal = next.filterBy(new int[]{1});
 //        }
 
-        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .layerFactory(LayerFactories.getFactory(RBM.class))
                 .constrainGradientToUnitNorm(true)
                 .iterationListener(new ScoreIterationListener(1))
-                .learningRate(1e-3)
+                .learningRate(1e-1)
                 .visibleUnit(RBM.VisibleUnit.GAUSSIAN)
                 .hiddenUnit(RBM.HiddenUnit.RECTIFIED)
                 .weightInit(WeightInit.DISTRIBUTION)
-                .dist(Nd4j.getDistributions().createNormal(1e-5, 1e-4))
+                .dist(Nd4j.getDistributions().createNormal(1e-5, 1))
+                .activationFunction("tanh")
                 .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
                 .nIn(rows * cols)
                 .nOut((int) (rows * cols * 0.75))
+                .list(4)
+                .override(3, new ClassifierOverride(3))
+                .hiddenLayerSizes((int) (rows * cols * 0.75), (int) (rows * cols * 0.75), (int) (rows * cols * 0.75))
+                .inputPreProcessor(0, new ZeroMeanAndUnitVariancePrePreProcessor())
                 .build();
 
 
         System.out.println(next.numExamples());
-        Layer rbm = conf.getLayerFactory().create(conf);
 
-        rbm.fit(next.getFeatureMatrix());
-//
-//        while (iter.hasNext()) {
-//
-//        }
+        MultiLayerNetwork network = new MultiLayerNetwork(conf);
+        Layer transporsed = network.getOutputLayer().transpose();
+        network.fit(next);
     }
 
 
